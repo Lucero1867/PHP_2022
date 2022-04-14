1ae24a0469094636296cbefa1ee8de680ce2f4be
lucero1867 <luceroolivos@gmail.com>
2022-04-14 09:13:05 -0400

    incorporacion de ejercicios nuevos

--- Hora_futura.php
+++ Hora_futura.php
@@ -3,5 +3,11 @@
 $hora_actual = readline("Hora actual: ");
 $cantidad_horas = readline("Cantidad de horas: ");
 
+function hora_futura($hora_actual, $cantidad_horas){
+    $dia = 24;
+    $resultado = ($hora_actual + $cantidad_horas) % $dia;
+    return $resultado;
+}
 
+echo "En " . $cantidad_horas . " horas, ". " el reloj marcara las ". hora_futura($hora_actual, $cantidad_horas);
 ?> 
---
+++ Parte_decimal.php
@@ -0,0 +1,7 @@
+<?php
+
+$numero = readline("Ingrese un numero: ");
+$decimal = explode('.', $numero);
+
+echo "0." . $decimal[1];
+?> 
